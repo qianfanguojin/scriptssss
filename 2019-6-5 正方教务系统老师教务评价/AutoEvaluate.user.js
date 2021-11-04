5369e79cb1e1f3574341b8fbdf3157d5d9a7d08c
xie0619 <1657098617@qq.com>
2021-11-04 12:42:15 +0800

    fix: 修复正方教务评教油猴安装的问题

--- 2019-6-5 正方教务系统老师教务评价/AutoEvaluate.user.js
+++ 2019-6-5 正方教务系统老师教务评价/AutoEvaluate.user.js
@@ -1,8 +1,22 @@
-//从第二行开始
-var i = 2;
-var item = [];
-var saveButton;
-for(var j = 0 ; j < 13 ; j++){
+// ==UserScript==
+// @name         测试脚本2
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *
+// @icon         none
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    //从第二行开始
+    var i = 2;
+    var item = [];
+    var saveButton;
+    for(var j = 0 ; j < 13 ; j++){
     
     item[j] = document.getElementById("DataGrid1__ctl" + i + "_JS1"); 
   
@@ -21,3 +35,7 @@ for(var j = 0 ; j < 13 ; j++){
    
     i++;
 }
+
+
+
+})();
